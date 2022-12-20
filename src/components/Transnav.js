2aca478bc8af0999fba3a0892cf80999b6aabc15
niteeshchaudhary <nkchaudhary00@gmail.com>
2022-12-20 23:42:07 +0530

    more addon

---
+++ src/components/Transnav.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./navestyle.css";
+export default function Transnav() {
+  return (
+    <div className="navbar">
+      <p>Home</p>
+      <p>Projects</p>
+      <p>Experience</p>
+    </div>
+  );
+}
---
+++ src/components/personalinfo/Perinfo.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "../block.css";
+
+export default function Perinfo() {
+  return (
+    <div className="blocks">
+      <div className="imgholder">
+        <img
+          src="https://avatars.githubusercontent.com/u/66108270?v=4"
+          alt="profile Pic"
+        />
+      </div>
+      <div class="textholder">
+        <h1>Niteesh Kamal Chaudhary</h1>
+        <h2>Software Developer</h2>
+        <p>
+          I am an aspiring software engineer who enjoys solving puzzles and
+          problems. I have strong technical skills and an academic background in
+          engineering, coding and development. I'm graduating in May 2024, and
+          interested in internship roles in software development, full stack
+          development and game development. Please feel free to get in touch
+          with me via email at nkchaudhary00@gmail.com
+        </p>
+      </div>
+    </div>
+  );
+}
