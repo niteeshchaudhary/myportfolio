93e1127485d1c6b4b46d7fd3107f087a918ee910
niteeshchaudhary <nkchaudhary00@gmail.com>
2023-08-15 20:25:43 +0530

    removed extra wf

--- src/components/personalinfo/Portfolio.js
+++ src/components/personalinfo/Portfolio.js
@@ -2,8 +2,11 @@ import { useEffect } from "react";
 import Card1 from "../cards/Cardt1";
 export default function Portfolio() {
   return (
-    <div className="portfoliocontainer" id="projects">
-      <Card1 />
-    </div>
+    <section id="projects">
+      <h2 className="heading">Projects</h2>
+      <div className="portfoliocontainer">
+        <Card1 />
+      </div>
+    </section>
   );
 }
