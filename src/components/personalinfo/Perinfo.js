1bdfc164b464830718d218a78c2abd0a30c766c0
Niteesh Kamal Chaudhary <nkchaudhary00@gmail.com>
2024-05-14 12:23:44 +0530

    made about dynamic

--- src/components/personalinfo/Perinfo.js
+++ src/components/personalinfo/Perinfo.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 
 import "../block.css";
+import { getDatabase, ref, child, get } from "firebase/database";
 
 export default function Perinfo() {
+  const [data, setData] = useState(`I am an aspiring software engineer who enjoys solving puzzles and
+  problems. I have strong technical skills and an academic background in
+  engineering, coding and development. I'm 
+  interested in internship roles in software development, full stack
+  development and game development. Please feel free to get in touch
+  with me via email at nkchaudhary00@gmail.com`);
+
+  useEffect(() => {
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `about`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          setData(snapshot.val());
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <div className="blocks" id="About">
       <div className="imgholder">
@@ -15,12 +37,7 @@ export default function Perinfo() {
         <h1>Niteesh Kamal Chaudhary</h1>
         <h2>Software Developer</h2>
         <p>
-          I am an aspiring software engineer who enjoys solving puzzles and
-          problems. I have strong technical skills and an academic background in
-          engineering, coding and development. I'm graduating in May 2024, and
-          interested in internship roles in software development, full stack
-          development and game development. Please feel free to get in touch
-          with me via email at nkchaudhary00@gmail.com
+          {data}
         </p>
       </div>
     </div>
